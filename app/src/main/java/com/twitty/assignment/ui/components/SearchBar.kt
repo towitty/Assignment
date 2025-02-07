c7d1bb172ad7eae18eeb7545c3495d5cf2b308be
Kim Young June <hot6tweety@gmail.com>
2025-02-07 17:06:16 +0900

    feat: SearchBar UI 컴포넌트 구현

---
+++ app/src/main/java/com/twitty/assignment/ui/components/SearchBar.kt
@@ -0,0 +1,47 @@
+package com.twitty.assignment.ui.components
+
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
+import com.twitty.assignment.R
+import com.twitty.assignment.ui.theme.AppIcons
+
+@Composable
+fun SearchBar(
+    onSearch: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    var searchText by remember { mutableStateOf("") }
+
+    OutlinedTextField(
+        value = searchText,
+        onValueChange = { searchText = it },
+        trailingIcon = {
+            IconButton(onClick = onSearch) {
+                Icon(imageVector = AppIcons.Search, contentDescription = stringResource(id = R.string.search))
+            }
+        },
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
+        keyboardActions = KeyboardActions(onSearch = { onSearch() }),
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewSearchBar() {
+    SearchBar(
+        onSearch = {}
+    )
+}
--- app/src/main/java/com/twitty/assignment/ui/search/SearchScreen.kt
+++ app/src/main/java/com/twitty/assignment/ui/search/SearchScreen.kt
@@ -1,14 +1,39 @@
 package com.twitty.assignment.ui.search
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.res.stringResource
-import com.twitty.assignment.R
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.twitty.assignment.ui.components.ExpandableBookItem
+import com.twitty.assignment.ui.components.SearchBar
 
 @Composable
 fun SearchScreen() {
-    Surface {
-        Text(stringResource(id = R.string.search))
+    Surface(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column {
+            SearchBar(
+                onSearch = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            )
+            LazyColumn(
+                modifier = Modifier
+                    .padding(start = 16.dp, end = 16.dp)
+            ) {
+                repeat(20) {
+                    item {
+                        ExpandableBookItem { }
+                    }
+                }
+            }
+        }
     }
 }

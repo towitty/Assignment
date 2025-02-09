92325f8e7709625920d13e5a110e9787e3daa014
Kim Young June <hot6tweety@gmail.com>
2025-02-09 19:30:06 +0900

    chore: Mockito 테스트 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -104,6 +104,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    testImplementation(libs.mockito.inline)
     testImplementation(libs.kotlinx.coroutines.test)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
